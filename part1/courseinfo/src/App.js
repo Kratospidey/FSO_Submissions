961974da07f79ebbe24a4cff037815e5f9f456be
kratospidey <paramsinghmakwana@gmail.com>
2023-06-27 19:52:48 +0530

    finish 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,19 +3,25 @@ import Header from "./components/Header";
 import Total from "./components/Total";
 
 const App = () => {
-	const parts = [
-		{ part: "Fundamentals of React", exercise: 10 },
-		{ part: "Using props to pass data", exercise: 7 },
-		{ part: "State of a component", exercise: 14 },
-	];
+	const course = "Half Stack application development";
+	const part1 = "Fundamentals of React";
+	const exercises1 = 10;
+	const part2 = "Using props to pass data";
+	const exercises2 = 7;
+	const part3 = "State of a component";
+	const exercises3 = 14;
 
 	return (
 		<div>
 			<Header course={course}></Header>
-			<Content part={part1} exercise={exercises1}></Content>
-			<Content part={part2} exercise={exercises2}></Content>
-			<Content part={part3} exercise={exercises3}></Content>
-			<Content></Content>
+			<Content
+				part1={part1}
+				exercises1={exercises1}
+				part2={part2}
+				exercises2={exercises2}
+				part3={part3}
+				exercises3={exercises3}
+			></Content>
 			<Total total={exercises1 + exercises2 + exercises3}></Total>
 		</div>
 	);
